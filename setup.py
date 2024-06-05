4fcd7cf9f29be2391a802d95d5ce20d8c383703a
Jarett <--global>
2024-06-04 19:23:19 -0700

    seems to be working a bit better whne using sudo? not sure.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from ez_setup import use_setuptools
-use_setuptools()
 
-from setuptools import setup, find_packages
+from archimedes import use_merlin
+use_merlin()
+
+from merlin import setup, find_packages
 
 setup(
     
